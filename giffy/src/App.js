717ae724cdd4ab6c1726c38a2b1c15e366847dd5
Andrés Rosillo <andresrosillo94@hotmail.com>
2020-08-09 19:02:04 +0200

    use efect hook, se ejecuta siempre al renderizar nuestra aplicación

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,11 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const GIFS = ["tal", "qual"];
-const DIFFERENT_GIFS = ["bueno", "vale"];
+const GIFS = [1, 2];
+const DIFFERENT_GIFS = [3, 4];
 
 function App() {
   const [gifs, setGifs] = useState(GIFS);
+
+  // se ejecuta al renderizar
+  // 2nd param, dependencias que tiene el efecto
+  useEffect(function () {
+    setGifs(DIFFERENT_GIFS)
+    console.log('efecto ejecutado')
+  }, []);
   /*const state = useState([]);
   const gifs = state[0];
   const setGifs = state[1];*/
@@ -15,9 +22,6 @@ function App() {
         {
           gifs.map(singleGif => <h3>{singleGif}</h3>)
         }
-        <button onClick={() => setGifs(DIFFERENT_GIFS)}>
-          Cambiar gifs
-        </button>
       </section>
     </div>
   );
